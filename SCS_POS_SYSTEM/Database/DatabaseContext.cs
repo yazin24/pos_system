8ddbb5cd04ff885186082fb2bb250ef0f0e7e0e4
yazin24 <yazinconiendo@gmail.com>
2024-02-19 19:52:13 +0800

    Add project files.

---
+++ SCS_POS_SYSTEM/Database/DatabaseContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SCS_POS_SYSTEM.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Entites here
+
+
+
+
+        //Relationship here
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string ConnectionString = _configuration.GetConnectionString("SqlConnection");
+
+            if(ConnectionString != null)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }else
+            {
+                Console.WriteLine("Cant connect to the Sql Server! Please try again later.");
+            }
+        }
+    }
+}
---
+++ SCS_POS_SYSTEM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SCS_POS_SYSTEM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
